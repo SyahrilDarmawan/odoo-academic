bfcb012115a47773a705631268dba7bb0293db83
Muhammad Syahril Darmawan <123642882+SyahrilDarmawan@users.noreply.github.com>
2025-03-19 09:17:07 +0700

    Delete odoo-academic directory

--- odoo-academic/academic/controller.py
+++
@@ -1,7 +0,0 @@
-from odoo import http
-
-
-class main(http.Controller):
-    @http.route('/web/view/edit_custom', type='json', auth="user")
-    def edit_custom(self, arch):
-        return {'result': True}
